ef0585e819dc3b4979776961d5aefedcdd3ce24f
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2022-11-06 15:27:53 +0100

    add ferret version

--- versions.sh
+++ versions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ -z "$FERRET_VERSION" && -z "$_FERRET_VERSION"  ]]; then
+if [ -z "$FERRET_VERSION" ]; then
   if [ $1 = 'ferret' ]; then
     echo $(cat go.mod | grep 'github.com/MontFerret/ferret v' | awk -F 'v' '{print $2}')
   else
     echo $(git describe --tags --always --dirty)
   fi
 else 
-  echo $FERRET_VERSION$_FERRET_VERSION
+  echo $FERRET_VERSION
 fi
