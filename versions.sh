0eae3edc4314566768c27f241e23777da5342485
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2022-11-06 16:34:27 +0100

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z "$FERRET_VERSION" ]; then
-  if [ $1 = 'ferret' ]; then
+if [ $1 = 'ferret' ]; then
+  if [ -z "$FERRET_VERSION" ]; then
     echo $(cat go.mod | grep 'github.com/MontFerret/ferret v' | awk -F 'v' '{print $2}')
-  else
-    echo $(git describe --tags --always --dirty)
+  else 
+    echo $FERRET_VERSION
   fi
-else 
-  echo $FERRET_VERSION
+else
+  echo $(git describe --tags --always --dirty)
 fi
